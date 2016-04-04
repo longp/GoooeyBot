288b055eaa4cf84ea2633f40d3d6d25f502ba744
longp <lphan1989@gmail.com>
2016-04-03 23:02:52 -0400

    added oath token and options object

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 var tmi = require('tmi.js');
+var oath = require('keys.js');
+var options = {
+  options : {
+    debug:true
+  },
+  connection:{
+    cluster:'aws',
+    reconnect:true
+  },
+  identity: {
+    username: 'goooeybot',
+    password: 'oath'
+  },
+  channels:['g000ey']
+};
---
+++ keys.js
@@ -0,0 +1,3 @@
+oath = 'oauth:m0waex80kd0s3krr0d5704ic52m1ax'
+
+module.exports = oath;
